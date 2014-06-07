86c434f3a954f006dbe1871b84743dcb6cf0b718
Faruk Brbovic <darkgaro@me.com>
2014-06-07 17:45:59 +0000

    changed the stub generator so it generates individual directories

--- Library/Stubs/Generator.php
+++ Library/Stubs/Generator.php
@@ -74,12 +74,13 @@ class Generator
             $source = $this->buildClass($class);
 
             $filename = ucfirst($class->getName()) . '.zep.php';
-            
+
             //$filePath = $path . str_replace($namespace, '', str_replace( '\\', '/', $class->getNamespace()));
             
             $filePath = $path . str_replace($namespace, '', str_replace($namespace . '\\\\', DIRECTORY_SEPARATOR, strtolower($class->getNamespace())));
             $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $filePath);
             $filePath = str_replace(DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $filePath);
+
 
             if (!is_dir($filePath)) {
                 mkdir($filePath, 0777, true);
